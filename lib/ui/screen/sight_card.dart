479bc072e0a9acb23aea194e782855cd3bd87e2d
alexbozhko-flutter <74686650+alexbozhko-flutter@users.noreply.github.com>
2021-01-18 03:33:48 +0200

    - убраны хвосты от предыдущих заданий + Выпонено: В виджете SightCard используйте ConstrainedBox, чтобы ограничить размер текста  по ширине до половины размера карточки. Посмотрите на результат. Почему размер виджета Text поменялся? После выполнения,  отрегулируйте размер текста согласно дизайну. Добавьте отступ между между фотографиями и описанием с помощью SizedBox AspectRatio используйте, чтобы привести виджеты SightCard в соотношение 3/2 Переверстать AppBar через наследника PreferredSizeWidget.

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -17,101 +17,121 @@ class SightCard extends StatelessWidget {
       return context.runtimeType;
     }
 
+    // Ширина экрана
+    var scrWidth = MediaQuery.of(context).size.width;
 
-    return
-
-      Padding(
-        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-        child: Column(
-
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(
+          AspectRatio(
+            aspectRatio: 3 / 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
                   top: Radius.circular(16),
-            ),
-              color: Color(0xFF801E48),
-            ),
-            // color: Color(0xFF801E48),
-            height: 96.0,
-            alignment: Alignment.topLeft, // !просмотреть
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 16,
-                  top: 16,
-                  child: //Text.rich(
-                  // Тип (Type) достопримечательности
-                  Text.rich(
-                      TextSpan(
-                        style: styleSightType,
-                        text: sight.type,
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                    )
                 ),
-                Positioned(
+                color: Color(0xFF801E48),
+              ),
+              // color: Color(0xFF801E48),
+              // height: 96.0,
+              alignment: Alignment.topLeft,
+              child: Stack(
+                children: [
+                  Positioned(
+                      left: 16,
+                      top: 16,
+                      child: //Text.rich(
+                          // Тип (Type) достопримечательности
+                          RichText(
+                        text: TextSpan(
+                          style: styleSightType,
+                          text: sight.type,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                      )),
+                  Positioned(
                     right: 16,
                     top: 16,
                     child: //Text.rich(
-                  Container( // иконка (заглушка)
-                    color: Colors.white,
-                    width: 24.0,
-                    height: 24.0,
+                        Container(
+                      // иконка (заглушка)
+                      color: Colors.white,
+                      width: 24.0,
+                      height: 24.0,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(16),
+          AspectRatio(
+            aspectRatio: 3 / 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(16),
+                ),
+                color: Color(0xFFF5F5F5),
               ),
-              color: Color(0xFFF5F5F5),
-            ),
-            height: 92.0,
-            child: Stack(
-              children: [
-                Positioned(
-                    left: 16,
-                    right: 16,
-                    top: 16,
-                    child:
-                    // Название (name) достопримечательности
-                    RichText(
-                      text: TextSpan(
-                        style: styleSightName,
-                        text: sight.name,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 16,
+                      child: Container(),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: ConstrainedBox(
+                        constraints:
+                            BoxConstraints(maxWidth: scrWidth / 2 - 32),
+                        child: Container(
+                          color: Color(0xffC4C4C4),
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 3.0,
+                              top: 2.0,
+                              right: 9.0,
+                            ),
+                            child: RichText(
+                              text: TextSpan(
+                                style: styleSightName,
+                                text: sight.name,
+                              ),
+                              maxLines: 3, // ограничение в 3 строки
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ),
                       ),
-                      maxLines: 2, // ограничение в 3 строки
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                    )
-                ),
-                Positioned(
-                    left: 16,
-                    right: 16,
-                    top: 58,
-                    child: //Text.rich(
-                    // Описание (details) достопримечательности
-                    Text.rich(
-                      TextSpan(
-                        style: styleSightDetails,
-                        text: sight.details,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 2.0),
+                        child: RichText(
+                          text: TextSpan(
+                            style: styleSightDetails,
+                            // text: sight.details,
+                            text: "закрыто до 20:00",
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                        ),
                       ),
-
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.left,
-                    )
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
-    ),
-      );
+      ),
+    );
   }
 }
--- lib/ui/screen/sight_list_sreen.dart
+++ lib/ui/screen/sight_list_sreen.dart
@@ -29,6 +29,44 @@ TextSpan appBarTitle = TextSpan(
   text: 'Список \nинтересных мест',
 );
 
+const barHeight = 72;
+
+/// Кастомный AppBar
+class SightListAppBar extends StatefulWidget implements PreferredSizeWidget {
+  SightListAppBar({Key key})
+      : preferredSize = Size.fromHeight(72), // константу или выражение сюда подставить нельзя
+        super(key: key);
+
+  @override
+  final Size preferredSize; // default is 56.0
+
+  @override
+  _SightListAppBarState createState() => _SightListAppBarState();
+}
+
+class _SightListAppBarState extends State<SightListAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white, // status bar color
+      brightness: Brightness.light, // status bar brightness
+      toolbarHeight: MediaQuery.of(context).padding.top + barHeight,
+      elevation: 0.0, //убирает тень appbar'а
+      title: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          child: RichText(
+            text: appBarTitle,
+            maxLines: 2, // ограничение в 3 строки
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.left,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 /// StatefulWidget для задания 4.2
 class SightListScreen extends StatefulWidget {
   @override
@@ -41,19 +79,9 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white, // status bar color
-        brightness: Brightness.light, // status bar brightness
-        toolbarHeight: 88,
-        elevation: 0.0, //убирает тень appbar'а
-        title:
-            RichText(
-          text: appBarTitle,
-          maxLines: 2, // ограничение в 3 строки
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.left,
-        ),
-      ),
+
+      appBar: SightListAppBar(),
+
       body: SingleChildScrollView(
         child: Column(
           children: [
